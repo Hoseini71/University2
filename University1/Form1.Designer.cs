1ff91a6d9449490e401c57fa84a9f11a0f071b11
massimo1365 <36961119+massimo1365@users.noreply.github.com>
2024-08-07 14:58:43 +0330

    Add project files.

---
+++ University1/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿
+namespace University
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNumber1 = new System.Windows.Forms.Button();
+            this.btnNumber2 = new System.Windows.Forms.Button();
+            this.dtGridview = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.txtBox2 = new System.Windows.Forms.TextBox();
+            this.txtBox3 = new System.Windows.Forms.TextBox();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridview)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("B Titr", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(777, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 26);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "نام درس";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("B Titr", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(784, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 26);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "مدرس";
+            // 
+            // btnNumber1
+            // 
+            this.btnNumber1.BackColor = System.Drawing.Color.Khaki;
+            this.btnNumber1.Font = new System.Drawing.Font("B Titr", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnNumber1.Location = new System.Drawing.Point(15, 88);
+            this.btnNumber1.Name = "btnNumber1";
+            this.btnNumber1.Size = new System.Drawing.Size(104, 59);
+            this.btnNumber1.TabIndex = 4;
+            this.btnNumber1.Text = "ذخیره";
+            this.btnNumber1.UseVisualStyleBackColor = false;
+            this.btnNumber1.Click += new System.EventHandler(this.btnNumber1_Click);
+            // 
+            // btnNumber2
+            // 
+            this.btnNumber2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.btnNumber2.Font = new System.Drawing.Font("B Titr", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnNumber2.Location = new System.Drawing.Point(654, 104);
+            this.btnNumber2.Name = "btnNumber2";
+            this.btnNumber2.Size = new System.Drawing.Size(104, 59);
+            this.btnNumber2.TabIndex = 5;
+            this.btnNumber2.Text = "نمایش";
+            this.btnNumber2.UseVisualStyleBackColor = false;
+            this.btnNumber2.Click += new System.EventHandler(this.btnNumber2_Click);
+            // 
+            // dtGridview
+            // 
+            this.dtGridview.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGridview.Location = new System.Drawing.Point(12, 237);
+            this.dtGridview.Name = "dtGridview";
+            this.dtGridview.Size = new System.Drawing.Size(836, 417);
+            this.dtGridview.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtBox1);
+            this.groupBox1.Controls.Add(this.txtBox2);
+            this.groupBox1.Controls.Add(this.txtBox3);
+            this.groupBox1.Controls.Add(this.btnNumber1);
+            this.groupBox1.Controls.Add(this.lbl3);
+            this.groupBox1.Font = new System.Drawing.Font("B Titr", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.groupBox1.Location = new System.Drawing.Point(31, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(493, 153);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ثبت استاد جدید";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("B Titr", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lbl4.Location = new System.Drawing.Point(416, 107);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(60, 26);
+            this.lbl4.TabIndex = 1;
+            this.lbl4.Text = "نام درس";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("B Titr", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(392, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 26);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "نام خانوادگی";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Location = new System.Drawing.Point(188, 102);
+            this.txtBox1.Multiline = true;
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(163, 38);
+            this.txtBox1.TabIndex = 2;
+            this.txtBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtBox2
+            // 
+            this.txtBox2.Location = new System.Drawing.Point(188, 15);
+            this.txtBox2.Multiline = true;
+            this.txtBox2.Name = "txtBox2";
+            this.txtBox2.Size = new System.Drawing.Size(163, 39);
+            this.txtBox2.TabIndex = 3;
+            this.txtBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtBox3
+            // 
+            this.txtBox3.Location = new System.Drawing.Point(188, 58);
+            this.txtBox3.Multiline = true;
+            this.txtBox3.Name = "txtBox3";
+            this.txtBox3.Size = new System.Drawing.Size(163, 38);
+            this.txtBox3.TabIndex = 5;
+            this.txtBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("B Titr", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.lbl3.Location = new System.Drawing.Point(442, 16);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(34, 26);
+            this.lbl3.TabIndex = 0;
+            this.lbl3.Text = "نام  ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(599, 65);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(159, 21);
+            this.comboBox1.TabIndex = 9;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(599, 27);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(159, 21);
+            this.comboBox3.TabIndex = 10;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 682);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dtGridview);
+            this.Controls.Add(this.btnNumber2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "انتخاب واحد";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridview)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNumber1;
+        private System.Windows.Forms.Button btnNumber2;
+        private System.Windows.Forms.DataGridView dtGridview;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtBox2;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+    }
+}
+
---
+++ University1/Form1.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using University1;
+
+namespace University
+{
+    public partial class Form1 : Form
+    {
+        private readonly object dataGridView;
+        int _affectedRow = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+
+            fillCombos();
+
+        }
+
+        public async void fillCombos()
+        {
+            string query;
+            query = @"select ID , Title from university.[dbo].[tblLesson] order by ID ";
+            DataTable dtUniversity = new DataTable();
+            dtUniversity = await showData(query);
+
+            comboBox1.DataSource = dtUniversity;
+            comboBox1.DisplayMember = dtUniversity.Columns[1].ToString();
+            comboBox1.ValueMember = dtUniversity.Columns[0].ToString();
+
+
+            query = @" select ID,FirstName +  LastName  from university.[dbo].[tblMaster] order by ID";
+            DataTable dtUniversity1 = new DataTable();
+            dtUniversity1 = await showData(query);
+            comboBox3.DataSource = dtUniversity1;
+            comboBox3.DisplayMember = dtUniversity1.Columns[1].ToString();
+            comboBox3.ValueMember = dtUniversity1.Columns[0].ToString();
+
+        }
+
+        private async void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async Task<DataTable> showData(string query)
+        {
+            try
+            {
+                // تعریف کانکشن جهت اتصال به  دیتابیس
+                var ConnectionString = "Server=localhost,1433; Initial Catalog = university ; Persist Security Info = False; User ID = Ali2; Password = 123456q@;";
+                SqlConnection _sqlConnection = new SqlConnection(ConnectionString);
+                SqlCommand _sqlCommand = new SqlCommand();
+                _sqlCommand.Connection = _sqlConnection;
+                SqlDataAdapter _sqlDataAdapter = new SqlDataAdapter(_sqlCommand);
+                var res = _sqlConnection.State;
+                if (res == ConnectionState.Closed)
+                {
+                    _sqlConnection.Open();
+                }
+
+                _sqlCommand.CommandType = CommandType.Text;
+                _sqlCommand.CommandText = query;
+
+                DataTable dt = new DataTable();
+                //DataSet ds = new DataSet();
+                _sqlDataAdapter.Fill(dt);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+
+        private async Task<DataTable> showLimitedTeacher(int indexId)
+        {
+            try
+            {
+                // تعریف کانکشن جهت اتصال به  دیتابیس
+                var ConnectionString = "Server=localhost,1433; Initial Catalog = university ; Persist Security Info = False; User ID = Ali2; Password = 123456q@;";
+                SqlConnection _sqlConnection = new SqlConnection(ConnectionString);
+                SqlCommand _sqlCommand = new SqlCommand();
+                _sqlCommand.Connection = _sqlConnection;
+                SqlDataAdapter _sqlDataAdapter = new SqlDataAdapter(_sqlCommand);
+                var res = _sqlConnection.State;
+                if (res == ConnectionState.Closed)
+                {
+                    _sqlConnection.Open();
+                }
+                string query = $@"SELECT FirstName +' '+ MT.LastName N'نام و نام خانوادگی',LS.Title N'نام درس قابل ارائه',LS.Code N'کد درس', LS.UnitNum N'تعداد واحد'
+                                FROM dbo.tblMaster MT
+                                INNER JOIN  [dbo].[Relation] RT  ON MT.ID = RT.MasterID
+                                INNER JOIN dbo.tblLesson LS ON LS.ID = RT.LESSONID
+                                where MT.ID  = {indexId} ";
+
+                _sqlCommand.CommandType = CommandType.Text;
+                _sqlCommand.CommandText = query;
+
+                DataTable dt = new DataTable();
+                _sqlDataAdapter.Fill(dt);
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+
+        private async void btnNumber2_Click(object sender, EventArgs e)
+        {
+            string query;
+            query = $@"SELECT M.FirstName, M.LastName, M.FatherName,M.BirthDay, L.Title  FROM [university]..Relation R INNER JOIN 
+                           university.[dbo].[tblLesson] L ON R.LessonID = L.ID
+						   INNER JOIN university.[dbo].[tblMaster] M ON R.MasterID = M.ID WHERE MasterID = {comboBox3.SelectedValue} ";
+            DataTable dtUniversity = new DataTable();
+            dtUniversity = await showData(query);
+            dtGridview.DataSource = dtUniversity;
+        }
+
+        private async Task<DataTable> showData(int indexId)
+        {
+            try
+            {
+                // تعریف کانکشن جهت اتصال به  دیتابیس
+                var ConectionString2 = "Server=localhost,1433; Initial Catalog = university ; Persist Security Info = False; User ID = Ali2; Password = 123456q@;";
+                // آدرس کانکشن را گرفته و  به آن وصل می شود
+                SqlConnection _sqlConnection = new SqlConnection(ConectionString2);
+                SqlCommand _sqlCommand = new SqlCommand();
+                _sqlCommand.Connection = _sqlConnection;
+                SqlDataAdapter _sqlDataAdapter = new SqlDataAdapter(_sqlCommand);
+
+                var res = _sqlConnection.State;
+                if (res == ConnectionState.Closed)
+                {
+                    _sqlConnection.Open();
+                }
+                // کوئری موجود در اسکیوئل که می خواهیم در پروزه خود فراخوانی و اجرا کنیم 
+                string query = $@"SELECT FirstName +' '+ MT.LastName N'نام و نام خانوادگی',LS.Title N'نام درس قابل ارائه',LS.Code N'کد درس', LS.UnitNum N'تعداد واحد'
+                                FROM dbo.tblMaster MT
+                                INNER JOIN  [dbo].[Relation] RT  ON MT.ID = RT.MasterID
+                                INNER JOIN dbo.tblLesson LS ON LS.ID = RT.LESSONID
+                                where MT.ID  = {indexId} ";
+
+                _sqlCommand.CommandType = CommandType.Text;
+                _sqlCommand.CommandText = query;
+
+                DataTable dt = new DataTable();
+                _sqlDataAdapter.Fill(dt);
+                return dt;
+                //var res2 = _sqlCommand.ExecuteReader();
+
+                //TestTmEntities testTm = new TestTmEntities();
+                //var salaries = testTm.CustomerInfoes.ToList();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+
+            }
+        }
+
+        private async void btnNumber1_Click(object sender, EventArgs e)
+        {
+            string firstName = txtBox2.Text;
+            string lastName = txtBox3.Text;
+            string Title = txtBox1.Text;
+            int cntInsert;
+            cntInsert = await SaveData(firstName, lastName , Title);
+            DataTable dtRes = new DataTable();
+            dtGridview.DataSource = dtRes;
+
+        }
+
+        private static async Task<int> SaveData(string FirstName, string LastName , string Title)
+        {
+            try
+            {
+                var ConectionString2 = "Server=localhost,1433; Initial Catalog = university ; Persist Security Info = False; User ID = Ali2; Password = 123456q@;";
+                SqlConnection _sqlConnection = new SqlConnection(ConectionString2);
+                SqlCommand _sqlCommand = new SqlCommand();
+                _sqlCommand.Connection = _sqlConnection;
+                SqlDataAdapter _sqlDataAdapter = new SqlDataAdapter(_sqlCommand);
+
+                var res = _sqlConnection.State;
+                if (res == ConnectionState.Closed)
+                {
+                    _sqlConnection.Open();
+                }
+
+                string query = $@"insert into [university].[dbo].[tblMaster](FirstName,LastName) 
+                    Values('{FirstName}','{LastName}') 
+                    insert into [university].[dbo].[tblLesson](Title) 
+                    Values('{Title}')";
+
+                _sqlCommand.CommandType = CommandType.Text;
+                _sqlCommand.CommandText = query;
+
+                return await _sqlCommand.ExecuteNonQueryAsync();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return -1;
+            }
+        }
+        private async Task<DataTable> showData()
+        {
+            try
+            {
+                // تعریف کانکشن جهت اتصال به  دیتابیس
+                var ConectionString2 = "Server=localhost,1433; Initial Catalog = university ; Persist Security Info = False; User ID = Ali2; Password = 123456q@;";
+                // آدرس کانکشن را گرفته و  به آن وصل می شود
+                SqlConnection _sqlConnection = new SqlConnection(ConectionString2);
+                SqlCommand _sqlCommand = new SqlCommand();
+                _sqlCommand.Connection = _sqlConnection;
+                SqlDataAdapter _sqlDataAdapter = new SqlDataAdapter(_sqlCommand);
+
+                var res = _sqlConnection.State;
+                if (res == ConnectionState.Closed)
+                {
+                    _sqlConnection.Open();
+                }
+                // کوئری موجود در اسکیوئل که می خواهیم در پروزه خود فراخوانی و اجرا کنیم 
+                string query = $@" Select [university].[dbo].[tblMaster](FirstName,LastName) 
+                    insert into [university].[dbo].[tblLesson](Title)";
+
+
+                _sqlCommand.CommandType = CommandType.Text;
+                _sqlCommand.CommandText = query;
+
+                DataTable dt = new DataTable();
+                _sqlDataAdapter.Fill(dt);
+                return dt;
+                //var res2 = _sqlCommand.ExecuteReader();
+
+                //TestTmEntities testTm = new TestTmEntities();
+                //var salaries = testTm.CustomerInfoes.ToList();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+
+            }
+        }
+
+        private async void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                await Task.Delay(100);
+
+                if (comboBox3.Items.Count >= 0)
+                {
+                    string query;
+                    query = $@"SELECT R.LessonID, L.Title  FROM [university]..Relation R INNER JOIN 
+                           university.[dbo].[tblLesson] L ON R.LessonID = L.ID WHERE MasterID = {comboBox3.SelectedValue} order by R.ID";
+                    DataTable dtUniversity = new DataTable();
+                    dtUniversity = await showData(query);
+
+                    comboBox1.DataSource = dtUniversity;
+                    comboBox1.DisplayMember = dtUniversity.Columns[1].ToString();
+                    comboBox1.ValueMember = dtUniversity.Columns[0].ToString();
+                }
+            }
+            catch { }
+            
+        }
+    }
+}
